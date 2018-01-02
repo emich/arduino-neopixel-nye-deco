377116d197767469b576c609acd4f47e7fdad731
Michael Uyttersprot <lemich@gmail.com>
2018-01-01 21:05:58 -0800

    Cleanup

--- nye-party-deco.ino
+++ nye-party-deco.ino
@@ -19,6 +19,7 @@ int cycle = 0;
 int numColorsToShow = 4;
 
 void setup() {
+  // Copypasta from Neopixel code samples. Not sure it makes a difference ¯\_(ツ)_/¯
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
@@ -27,7 +28,7 @@ void setup() {
 
   strip.begin();
   strip.setBrightness(brightness);
-  strip.show(); // Initialize all pixels to 'off'  
+  strip.show();
 
   Serial.begin(9600);
 }
